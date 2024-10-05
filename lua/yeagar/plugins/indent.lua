851268445842168ae06078e7da6a469a8a47d556
yeager@Phantom <meetparmar6656@gmail.com>
2024-10-04 20:59:02 -0400

    Edited indent line look

--- lua/yeagar/plugins/indent.lua
+++ lua/yeagar/plugins/indent.lua
@@ -1,13 +1,15 @@
 require("hlchunk").setup({
 	chunk = {
 		enable = true,
+		chars = {
+			right_arrow = "─",
+		},
 		style = {
 			{ fg = "#bdae93" },
 			{ fg = "#c21f30" },
 		},
 		duration = 0,
 		delay = 0,
-
 		-- ...
 	},
 	indent = {
